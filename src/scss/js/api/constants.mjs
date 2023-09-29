2fa6737099d6e43fa2bb9525168ce96925a1ed13
vanya992 <vanjacrv92@gmail.com>
2023-09-29 13:34:02 +0200

    basic setup of content

---
+++ src/scss/js/api/constants.mjs
@@ -0,0 +1,5 @@
+export const API_URL = "https://https://api.noroff.dev/api/v1";
+export const API_SOCIAL_BASE = "/social";
+export const API_AUTH = "/social/auth/register";
+export const API_BASE_URL = `${API_URL}${API_SOCIAL_BASE}`;
+export const API_REGISTER = `${API_URL}${API_AUTH}`;
