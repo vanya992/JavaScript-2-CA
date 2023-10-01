464e866241fe5345e90bc6744faee2347a147e76
vanya992 <vanjacrv92@gmail.com>
2023-10-01 15:05:03 +0200

    fonts and icons setup

---
+++ js/forms/register.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("#registerForm")
+
+console.log("Hi")
+
+form.addEventListener("submit")
--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,3 @@
 import * as constants from "./api/constants.mjs"
 
-console.log(constants.API_SOCIAL_URL);
+console.log(constants.API_REGISTER);
