15a6b6ee729f2e7ece3de47e07554636287d841a
vanya992 <vanjacrv92@gmail.com>
2023-10-22 11:16:02 +0200

    create media added

--- scripts/posts/create.mjs
+++ scripts/posts/create.mjs
@@ -39,30 +39,31 @@ document.getElementById("postForm").addEventListener("submit", async (event) => 
 
     const postTitle = document.getElementById("postTitle").value;
     const postBody = document.getElementById("postBody").value;
+    const postMedia = document.getElementById("postMedia").value;
 
     try {
-        
-        const newPost = await createPost(token, { title: postTitle, body: postBody });
+        const newPost = await createPost(token, { title: postTitle, body: postBody, media: postMedia });
 
-       
-        const postsContainer = document.getElementById("posts")
+        const postsContainer = document.getElementById("posts");
         const postElement = document.createElement("div");
         postElement.innerHTML = `
             <h2>${newPost.title}</h2>
             <p>${newPost.body}</p>
+            <img src="${newPost.media}">
         `;
         postsContainer.appendChild(postElement);
 
-        setTimeout(function () {
-            location.reload();
-          }, 200);
-        document.getElementById("postTitle").value = "";
-        document.getElementById("postBody").value = "";
+         setTimeout(function () {
+         location.reload();
+         }, 200);
+            document.getElementById("postTitle").value = "";
+            document.getElementById("postBody").value = "";
+            document.getElementById("postMedia").value = "";
+       
     } catch (error) {
-        console.log(error);
+        console.error(error);
         alert("Error creating the post. Please try again.");
     }
-    
 });
 
 
